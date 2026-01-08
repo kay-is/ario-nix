e5072c5dc3a0c86a80b6bbdbd96b0fcfa9da88db
K <k@kay.is>
2026-01-08 14:23:21 +0100

    refactor: extract proxy config

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
     ./disk-config.nix
+    ./proxy-config.nix
   ];
 
   boot.loader.grub = {
@@ -25,41 +26,6 @@
   virtualisation.docker.enable = true;
   services.openssh.enable = true;
   services.glances.enable = true;
-
-  networking.firewall.allowedTCPPorts = [
-    80
-    443
-  ];
-
-  security.acme = {
-    acceptTerms = true;
-    defaults = {
-      group = "nginx";
-
-      dnsResolver = "1.1.1.1:53";
-      dnsPropagationCheck = true;
-
-      email = "fllstck@pm.me";
-    };
-
-    certs."permaframes.cc" = {
-      extraDomainNames = [ "*.permaframes.cc" ];
-    };
-  };
-
-  services.nginx = {
-    enable = true;
-    recommendedProxySettings = true;
-    recommendedTlsSettings = true;
-    virtualHosts."permaframes.cc" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://localhost:3000";
-        proxyWebsockets = true;
-      };
-    };
-  };
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDUgE09xB2WSFpuow24yGsgPoLaEVMFfYC+/S5p+mS69 k@k-lg-gram"
---
+++ proxy-config.nix
@@ -0,0 +1,55 @@
+{ config, pkgs, ... }:
+{
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      group = "nginx";
+
+      dnsResolver = "1.1.1.1:53";
+      dnsPropagationCheck = true;
+
+      email = "fllstck@pm.me";
+    };
+
+    certs."permaframes.cc" = {
+      extraDomainNames = [ "*.permaframes.cc" ];
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts."permaframes.cc" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://localhost:3000";
+        proxyWebsockets = true;
+      };
+      locations."/grafana/" = {
+        proxyPass = "http://localhost:1024/grafana";
+        proxyWebsockets = true;
+        basicAuth = {
+          users = {
+            "admin" = "permaframes123";
+          };
+        }
+      };
+      locations."/glances/" = {
+        proxyPass = "http://localhost:61208";
+        proxyWebsockets = true;
+        basicAuth = {
+          users = {
+            "admin" = "permaframes123";
+          };
+        }
+      };
+    };
+  };
+}
